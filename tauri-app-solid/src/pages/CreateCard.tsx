1484a23fe9bc31726c4c1a8a35b1d54ab98be53e
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2022-07-11 22:05:37 -0400

    card creation

--- tauri-app-solid/src/pages/CreateCard.tsx
+++ tauri-app-solid/src/pages/CreateCard.tsx
@@ -9,8 +9,8 @@ const CreateCard: Component<CreateCardProps> = (props: CreateCardProps) => {
 
   const [showAlert, setShowAlert] = createSignal<boolean>(false);
 
-  const add = () => {
-    setNum(num() + 2);
+  const createCard = () => {
+    alert("hu");
   };
 
   return (
@@ -23,34 +23,100 @@ const CreateCard: Component<CreateCardProps> = (props: CreateCardProps) => {
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <button onClick={() => setShowAlert(true)} class="btn btn-primary">
-            Show Alert
-          </button>
-          <button onClick={add} class="btn btn-primary">
-            Create Card!
-          </button>
           <p>{num()}</p>
         </div>
+        <section id="components">
+          <article class="my-3" id="validation">
+            <div>
+              <div class="bd-example">
+                <form class="row g-3">
+                  <div class="col-md-4">
+                    <label for="validationServer01" class="form-label">
+                      Native Language
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control is-valid"
+                      id="validationServer01"
+                      value=""
+                      required
+                    />
+                    <div class="valid-feedback">Looks good!</div>
+                  </div>
+                  <div class="col-md-4">
+                    <label for="validationServer02" class="form-label">
+                      Foreign Language
+                    </label>
+                    <div class="input-group has-validation">
+                      <span class="input-group-text" id="inputGroupPrepend3">
+                        @
+                      </span>
+                      <input
+                        type="text"
+                        class="form-control is-valid"
+                        id="validationServer02"
+                        value=""
+                        required
+                      />
 
-        {showAlert() && (
-          <div
-            class="alert alert-primary alert-dismissible fade show"
-            aria-role="alert"
-          >
-            A simple primary alert with{" "}
-            <a href="#" onClick={(e) => e.preventDefault()} class="alert-link">
-              an example link
-            </a>
-            . Give it a click if you like.
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="alert"
-              aria-label="Close"
-              onClick={() => setShowAlert(false)}
-            ></button>
-          </div>
-        )}
+                      <div class="invalid-feedback">
+                        Please choose a username.
+                      </div>
+                    </div>
+                    <div class="valid-feedback">Looks good!</div>
+                  </div>
+                  <div class="col-md-4">
+                    <label for="validationServerUsername" class="form-label">
+                      Username
+                    </label>
+                    <div class="input-group has-validation">
+                      <span class="input-group-text" id="inputGroupPrepend3">
+                        @
+                      </span>
+                      <input
+                        type="text"
+                        class="form-control is-invalid"
+                        id="validationServerUsername"
+                        aria-describedby="inputGroupPrepend3"
+                        required
+                      />
+                      <div class="invalid-feedback">
+                        Please choose a username.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <label for="validationServer04" class="form-label">
+                      State
+                    </label>
+                    <select
+                      class="form-select is-invalid"
+                      id="validationServer04"
+                      required
+                    >
+                      <option selected disabled value="">
+                        Choose...
+                      </option>
+                      <option>...</option>
+                    </select>
+                    <div class="invalid-feedback">
+                      Please select a valid state.
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button
+                      onClick={createCard}
+                      class="btn btn-primary"
+                      type="submit"
+                    >
+                      Create Card!
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </article>
+        </section>
       </div>
     </div>
   );
